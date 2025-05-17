afa97e9f597a6f760972aceb26874fe66e4f4520
Kooka Vicks <42599862-kookavicks82@users.noreply.replit.com>
2025-05-17 14:47:04 +0000

    Initial commit

---
+++ main/urls.py
@@ -0,0 +1,12 @@
+# main/urls.py
+
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('', views.home, name='home'),
+    path('login/', views.login_view, name='login'),
+    path('register/', views.register_view, name='register'),
+    path('dashboard/', views.dashboard, name='dashboard'),
+    path('upload/', views.upload_excel, name='upload'),
+]
